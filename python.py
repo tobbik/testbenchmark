3b56461fd815b0290b7f47e57379d810f921d0f0
Tobias Kieslich <tkieslich@gmail.com>
2015-07-06 14:26:42 -0700

    Fix the loop count in the python script

--- python.py
+++ python.py
@@ -6,8 +6,8 @@ i_counter = 0
 
 # From 0 to 31999
 for i_loop1 in range( 0, 10 ):
-    for i_loop2 in range( 0,31999 ):
-        for i_loop3 in range( 0,31999 ):
+    for i_loop2 in range( 0,32000 ):
+        for i_loop3 in range( 0,32000 ):
             i_counter += 1
             if ( i_counter > 50 ) :
                 i_counter = 0
